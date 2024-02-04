b929b11176968ea87ea3c273e7fbe9ca339d6dd6
Vladislav <bigmag174@yandex.ru>
2024-02-04 19:02:05 +0500

    Add Characters activity

---
+++ app/src/main/java/com/example/starwarsapi/Characters.kt
@@ -0,0 +1,11 @@
+package com.example.starwarsapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Characters : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_characters)
+    }
+}
